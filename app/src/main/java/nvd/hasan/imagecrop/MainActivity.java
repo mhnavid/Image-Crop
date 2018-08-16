f763a73081e3926d24ff607d0f12c38f7ea94fdf
mhnavid <hasan.nvd@gmail.com>
2018-08-16 21:53:54 +0600

    Initial commit

---
+++ app/src/main/java/nvd/hasan/imagecrop/MainActivity.java
@@ -0,0 +1,15 @@
+package nvd.hasan.imagecrop;
+
+import android.graphics.Camera;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.FrameLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
