39257deea2c540c10ef44422dd19b53997fc1327
mhnavid <hasan.nvd@gmail.com>
2018-08-18 04:41:16 +0600

    3

--- app/src/main/java/nvd/hasan/imagecrop/MainActivity.java
+++ app/src/main/java/nvd/hasan/imagecrop/MainActivity.java
@@ -4,15 +4,26 @@ import android.content.Intent;
 import android.graphics.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button takePhotoBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent in = new Intent(this, cameraShow.class);
-        startActivity(in);
         setContentView(R.layout.activity_main);
+
+        takePhotoBtn = findViewById(R.id.takePhotoBtn);
+        takePhotoBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(MainActivity.this, cameraShow.class);
+                startActivity(in);
+            }
+        });
     }
 }
